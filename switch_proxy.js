9b63b55d8184c69c98c51c4dc2f33f4baf1ee5e3
nickcxm <33414628+nickcxm@users.noreply.github.com>
2024-09-06 12:13:11 +0800

    Create switch_proxy.js

---
+++ switch_proxy.js
@@ -0,0 +1,17 @@
+var d = new Date();
+var h = d.getHours();
+
+const nightChoice = ["dmit49"];
+const dayChoice = ["dmit3949-auto"];
+
+var nodeList;
+console.log("切换开始");
+if (h >= 18 || h < 1) {
+    nodeList = nightChoice;
+} else {
+    nodeList = dayChoice;
+}
+
+const nodeName = nodeList[0];
+$surge.setSelectGroupPolicy("✈️ 节点选择", nodeName);
+$done();
