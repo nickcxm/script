f4150acf2ae15cd59e995e2fa638e6a03a9d19be
nickcxm <33414628+nickcxm@users.noreply.github.com>
2024-10-23 14:35:15 +0800

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -26,5 +26,7 @@ echo 'COMPLETION_WAITING_DOTS="true"' >> ~/.zshrc
 echo 'plugins=(git extract fasd zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
 echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
 
+# 立即应用更改
+source ~/.zshrc
 # 提示用户重启终端
 echo "Zsh 和 Oh My Zsh 已经安装完成，请重启终端以应用更改。"
